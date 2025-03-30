391dab17f75674785435ce2e74ab183315db1429
Kleb <youmustp@gmail.com>
2025-03-30 20:53:44 +1300

    First commit of files

---
+++ localization/default.lua
@@ -0,0 +1,84 @@
+return {
+    descriptions = {
+        Back={},
+        Blind={},
+        Edition={},
+        Enhanced={},
+        Joker={},
+        Other={
+            red_seal_joker = {
+                name="Red Seal",
+                text={
+                    "Retrigger this",
+                    "Joker {C:attention}1{} time",
+                },
+            },
+            purple_seal_joker = {
+                name="Purple Seal",
+                text={
+                    "Creates a {C:tarot}Tarot{} card",
+                    "when {C:attention}sold",
+                    "{C:inactive}(Must have room)",
+                },
+            },
+            gold_seal_joker = {
+                name="Gold Seal",
+                text={
+                    "Earn {C:money}$3{} when this",
+                    "Joker is scored",
+                },
+            },
+            blue_seal_joker = {
+                name="Blue Seal",
+                text={
+                    "Creates the {C:planet}Planet{} card",
+                    "for final played {C:attention}poker hand{}",
+                    "of round",
+                    "{C:inactive}(Must have room)",
+                },
+            },
+            cry_green_seal_joker = {
+				name = "Green Seal",
+				text = {
+					"Creates a {C:cry_code}Code{} card",
+					"when this Joker does not score",
+					"{C:inactive}(Must have room)",
+				},
+			},
+            cry_azure_seal_joker = {
+				name = "Azure Seal",
+				text = {
+					"Create {C:attention}3{} {C:dark_edition}Negative{}",
+					"{C:planet}Planets{} for played",
+					"{C:attention}poker hand{}, then",
+					"{C:red}destroy{} this Joker",
+				},
+			},
+        },
+        Planet={},
+        Spectral={},
+        Stake={},
+        Tag={},
+        Tarot={},
+        Voucher={},
+    },
+    misc = {
+        achievement_descriptions={},
+        achievement_names={},
+        blind_states={},
+        challenge_names={},
+        collabs={},
+        dictionary={},
+        high_scores={},
+        labels={},
+        poker_hand_descriptions={},
+        poker_hands={},
+        quips={},
+        ranks={},
+        suits_plural={},
+        suits_singular={},
+        tutorial={},
+        v_dictionary={},
+        v_text={},
+    },
+}
---
+++ main.lua
@@ -0,0 +1,486 @@
+SMODS.Atlas{
+    key = 'What',
+    path = 'What.png',
+    px = 71,
+    py = 95
+}
+
+SMODS.Atlas{
+    key = 'modicon',
+    path = 'modicon.png',
+    px = 34,
+    py = 34
+}
+
+SMODS.current_mod.optional_features = function()
+    return {
+        retrigger_joker = true,
+        post_trigger = true
+    }
+end
+
+local cryptidyeohna = false
+if next(SMODS.find_mod("Cryptid")) then
+    cryptidyeohna = true
+end
+
+SMODS.Consumable{
+    key = 'dejavu?',
+    set = 'Spectral',
+    atlas = 'What',
+    pos = {x = 0, y = 0},
+    loc_txt = {
+        name = 'Deja Vu?',
+        text = {
+            'Add a {C:red}Red Seal{}',
+            'to a random joker',
+        }
+    },
+    config = {mod_conv = "Red", cards = 1},
+    loc_vars = function(self,info_queue,center)
+        info_queue[#info_queue+1] = { key = "red_seal_joker", set = "Other"}
+    end,
+    unlocked = true,
+    discovered = true,
+    can_use = function(self,card)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        return #eligible > 0 and true or false
+    end,
+    use = function(self,card,area,copier)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        local highlighted = pseudorandom_element(eligible, pseudoseed('dejavu'))
+        if highlighted then
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    play_sound("tarot1")
+                    highlighted:juice_up(0.3, 0.5)
+                    return true
+                end,
+            }))
+            G.E_MANAGER:add_event(Event({
+                trigger = "after",
+                delay = 0.1,
+                func = function()
+                    if highlighted then
+                        highlighted:set_seal("Red")
+                    end
+                    return true
+                end,
+            }))
+        end
+    end
+}
+
+SMODS.Consumable{
+    key = 'trance?',
+    set = 'Spectral',
+    atlas = 'What',
+    pos = {x = 1, y = 0},
+    loc_txt = {
+        name = 'Trance?',
+        text = {
+            'Add a {C:blue}Blue Seal{}',
+            'to a random joker',
+        }
+    },
+    config = {mod_conv = "Blue", cards = 1},
+    loc_vars = function(self,info_queue,center)
+        info_queue[#info_queue+1] = { key = "blue_seal_joker", set = "Other"}
+    end,
+    unlocked = true,
+    discovered = true,
+    can_use = function(self,card)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        return #eligible > 0 and true or false
+    end,
+    use = function(self,card,area,copier)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        local highlighted = pseudorandom_element(eligible, pseudoseed('trance'))
+        if highlighted then
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    play_sound("tarot1")
+                    highlighted:juice_up(0.3, 0.5)
+                    return true
+                end,
+            }))
+            G.E_MANAGER:add_event(Event({
+                trigger = "after",
+                delay = 0.1,
+                func = function()
+                    if highlighted then
+                        highlighted:set_seal("Blue")
+                    end
+                    return true
+                end,
+            }))
+        end
+    end
+}
+
+SMODS.Consumable{
+    key = 'talisman?',
+    set = 'Spectral',
+    atlas = 'What',
+    pos = {x = 2, y = 0},
+    loc_txt = {
+        name = 'Talisman?',
+        text = {
+            'Add a {C:gold}Gold Seal{}',
+            'to a random joker',
+        }
+    },
+    config = {mod_conv = "Gold", cards = 1},
+    loc_vars = function(self,info_queue,center)
+        info_queue[#info_queue+1] = { key = "gold_seal_joker", set = "Other"}
+    end,
+    unlocked = true,
+    discovered = true,
+    can_use = function(self,card)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        return #eligible > 0 and true or false
+    end,
+    use = function(self,card,area,copier)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        local highlighted = pseudorandom_element(eligible, pseudoseed('talisman'))
+        if highlighted then
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    play_sound("tarot1")
+                    highlighted:juice_up(0.3, 0.5)
+                    return true
+                end,
+            }))
+            G.E_MANAGER:add_event(Event({
+                trigger = "after",
+                delay = 0.1,
+                func = function()
+                    if highlighted then
+                        highlighted:set_seal("Gold")
+                    end
+                    return true
+                end,
+            }))
+        end
+    end
+}
+
+SMODS.Consumable{
+    key = 'medium?',
+    set = 'Spectral',
+    atlas = 'What',
+    pos = {x = 3, y = 0},
+    loc_txt = {
+        name = 'Medium?',
+        text = {
+            'Add a {C:purple}Purple Seal{}',
+            'to a random joker',
+        }
+    },
+    config = {mod_conv = "Purple", cards = 1},
+    loc_vars = function(self,info_queue,center)
+        info_queue[#info_queue+1] = { key = "purple_seal_joker", set = "Other"}
+    end,
+    unlocked = true,
+    discovered = true,
+    can_use = function(self,card)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        return #eligible > 0 and true or false
+    end,
+    use = function(self,card,area,copier)
+        local eligible = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.seal then
+                eligible[#eligible + 1] = v
+            end
+        end
+        local highlighted = pseudorandom_element(eligible, pseudoseed('medium'))
+        if highlighted then
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    play_sound("tarot1")
+                    highlighted:juice_up(0.3, 0.5)
+                    return true
+                end,
+            }))
+            G.E_MANAGER:add_event(Event({
+                trigger = "after",
+                delay = 0.1,
+                func = function()
+                    if highlighted then
+                        highlighted:set_seal("Purple")
+                    end
+                    return true
+                end,
+            }))
+        end
+    end
+}
+
+if cryptidyeohna then
+    SMODS.Consumable{
+        key = 'typhoon?',
+        set = 'Spectral',
+        atlas = 'What',
+        pos = {x = 4, y = 0},
+        loc_txt = {
+            name = 'Typhoon?',
+            text = {
+                'Add a {C:cry_azure}Azure Seal{}',
+                'to a random joker',
+            }
+        },
+        config = {mod_conv = "cry_azure", cards = 1},
+        loc_vars = function(self,info_queue,center)
+            info_queue[#info_queue+1] = { key = "cry_azure_seal_joker", set = "Other"}
+        end,
+        unlocked = true,
+        discovered = true,
+        can_use = function(self,card)
+            local eligible = {}
+            for k, v in pairs(G.jokers.cards) do
+                if not v.seal then
+                    eligible[#eligible + 1] = v
+                end
+            end
+            return #eligible > 0 and true or false
+        end,
+        use = function(self,card,area,copier)
+            local eligible = {}
+            for k, v in pairs(G.jokers.cards) do
+                if not v.seal then
+                    eligible[#eligible + 1] = v
+                end
+            end
+            local highlighted = pseudorandom_element(eligible, pseudoseed('typhoon'))
+            if highlighted then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        play_sound("tarot1")
+                        highlighted:juice_up(0.3, 0.5)
+                        return true
+                    end,
+                }))
+                G.E_MANAGER:add_event(Event({
+                    trigger = "after",
+                    delay = 0.1,
+                    func = function()
+                        if highlighted then
+                            highlighted:set_seal("cry_azure")
+                        end
+                        return true
+                    end,
+                }))
+            end
+        end
+    }
+    SMODS.Consumable{
+        key = 'source?',
+        set = 'Spectral',
+        atlas = 'What',
+        pos = {x = 5, y = 0},
+        loc_txt = {
+            name = 'Source?',
+            text = {
+                'Add a {C:cry_code}Green Seal{}',
+                'to a random joker',
+            }
+        },
+        config = {mod_conv = "cry_green", cards = 1},
+        loc_vars = function(self,info_queue,center)
+            info_queue[#info_queue+1] = { key = "cry_green_seal_joker", set = "Other"}
+        end,
+        unlocked = true,
+        discovered = true,
+        can_use = function(self,card)
+            local eligible = {}
+            for k, v in pairs(G.jokers.cards) do
+                if not v.seal then
+                    eligible[#eligible + 1] = v
+                end
+            end
+            return #eligible > 0 and true or false
+        end,
+        use = function(self,card,area,copier)
+            local eligible = {}
+            for k, v in pairs(G.jokers.cards) do
+                if not v.seal then
+                    eligible[#eligible + 1] = v
+                end
+            end
+            local highlighted = pseudorandom_element(eligible, pseudoseed('source'))
+            if highlighted then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        play_sound("tarot1")
+                        highlighted:juice_up(0.3, 0.5)
+                        return true
+                    end,
+                }))
+                G.E_MANAGER:add_event(Event({
+                    trigger = "after",
+                    delay = 0.1,
+                    func = function()
+                        if highlighted then
+                            highlighted:set_seal("cry_green")
+                        end
+                        return true
+                    end,
+                }))
+            end
+        end
+    }
+end
+
+local oldcalcseal = Card.calculate_seal
+function Card:calculate_seal(context)
+    if self.debuff then return nil end
+    if self.ability and self.ability.set == 'Joker' then
+        if context.retrigger_joker_check and not context.retrigger_joker and self == context.other_card and self.seal == "Red" then
+            return {
+                repetitions = 1,
+                card = self
+            }
+        end
+        if self.seal == 'Blue' and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit and context.end_of_round and context.cardarea == G.jokers then
+            local card_type = 'Planet'
+            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                trigger = 'before',
+                delay = 0.0,
+                func = (function()
+                    if G.GAME.last_hand_played then
+                        local _planet = 0
+                        for k, v in pairs(G.P_CENTER_POOLS.Planet) do
+                            if v.config.hand_type == G.GAME.last_hand_played then
+                                _planet = v.key
+                            end
+                        end
+                        local card = create_card(card_type,G.consumeables, nil, nil, nil, nil, _planet, 'blusl')
+                        card:add_to_deck()
+                        G.consumeables:emplace(card)
+                        G.GAME.consumeable_buffer = 0
+                    end
+                    return true
+                end)}))
+            card_eval_status_text(self, 'extra', nil, nil, nil, {message = localize('k_plus_planet'), colour = G.C.SECONDARY_SET.Planet})
+        end
+        if self.seal == 'Purple' and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit and context.selling_self then
+            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+            G.E_MANAGER:add_event(Event({
+                trigger = 'before',
+                delay = 0.0,
+                func = (function()
+                        local card = create_card('Tarot',G.consumeables, nil, nil, nil, nil, nil, '8ba')
+                        card:add_to_deck()
+                        G.consumeables:emplace(card)
+                        G.GAME.consumeable_buffer = 0
+                    return true
+                end)}))
+            card_eval_status_text(self, 'extra', nil, nil, nil, {message = localize('k_plus_tarot'), colour = G.C.PURPLE})
+        end
+        if self.seal == 'Gold' and context.post_trigger and context.other_card == self then
+            return {
+                dollars = 3,
+                card = self,
+                message_card = self,
+            }
+        end
+        if cryptidyeohna then
+            if self.seal == 'cry_azure' and context.after then
+                G.E_MANAGER:add_event(Event({
+                    trigger = "before",
+                    delay = 0.0,
+                    func = function()
+                        local card_type = "Planet"
+                        local _planet = nil
+                        if G.GAME.last_hand_played then
+                            for k, v in pairs(G.P_CENTER_POOLS.Planet) do
+                                if v.config.hand_type == G.GAME.last_hand_played then
+                                    _planet = v.key
+                                    break
+                                end
+                            end
+                        end
+    
+                        for i = 1, 3 do
+                            local card = create_card(card_type, G.consumeables, nil, nil, nil, nil, _planet, "cry_azure")
+    
+                            card:set_edition({ negative = true }, true)
+                            card:add_to_deck()
+                            G.consumeables:emplace(card)
+                        end
+                        return true
+                    end,
+                }))
+                G.E_MANAGER:add_event(Event({
+                    trigger = "after",
+                    delay = 0.2,
+                    func = function()
+                        self:start_dissolve()
+                        return true
+                    end,
+                }))
+            end
+            if self.seal == 'cry_green' then
+                local scoredyeohna
+                if context.before then
+                    scoredyeohna = false
+                end
+                if context.post_trigger and context.other_card == self then
+                    scoredyeohna = true
+                end
+                if not scoredyeohna and context.after then
+                    G.E_MANAGER:add_event(Event({
+						func = function()
+							if G.consumeables.config.card_limit > #G.consumeables.cards then
+								local c = create_card("Code", G.consumeables, nil, nil, nil, nil, nil, "cry_green_seal")
+								c:add_to_deck()
+								G.consumeables:emplace(c)
+								self:juice_up()
+							end
+							return true
+						end,
+					}))
+                end
+            end
+        end
+    end
+    if self.ability.set == 'Joker' then return nil end
+    return oldcalcseal(self, context)
+end
